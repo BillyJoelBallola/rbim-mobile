5ad1c7496091b6e7b06612475101298990ad32c5
billy <billyjoelballola27@gmail.com>
2024-02-25 15:39:28 +0800

    change axios base url

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'http://192.168.100.238:4000/api',
+  baseURL: 'https://rbim-server.onrender.com/api',
   withCredentials: true
 })
